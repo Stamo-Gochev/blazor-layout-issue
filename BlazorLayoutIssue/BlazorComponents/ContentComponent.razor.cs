c7d7f7e41279fe1121d0a01d5146b24f92f5094c
Stamo-Gochev <stamo_gochev@yahoo.com>
2023-09-14 16:22:10 +0300

    feat: add sample project

---
+++ BlazorLayoutIssue/BlazorComponents/ContentComponent.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponents
+{
+    public partial class ContentComponent : ComponentBase, IDisposable
+    {
+        [CascadingParameter]
+        protected RootComponent RootComponent { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (RootComponent == null)
+            {
+                throw new Exception("Root component is not available");
+            }
+            else
+            {
+                RootComponent.InitializedComponents.Add(this);
+            }
+
+            await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ BlazorLayoutIssue/BlazorComponents/RootComponent.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponents
+{
+    public partial class RootComponent : ComponentBase, IDisposable
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        internal List<ComponentBase> InitializedComponents { get; set; } = new List<ComponentBase>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
